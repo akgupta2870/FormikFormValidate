9584897650e1eee77dfa54e7752caf4fede35bb8
akgupta2870 <82209087+akgupta2870@users.noreply.github.com>
2021-10-07 10:58:03 +0530

    styledCompoennt use Styled Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import { AboutUs } from "./AboutUs";
 import "./styles.css";
+import { Button, Box, Layer } from "grommet";
+import { useState } from "react";
+import styled from "styled-components";
+
+const StyledButton = styled(Button)`
+  width: 100px;
+  color: black;
+  background-color: violet;
+`;
 
 export default function App() {
+  const [show, setShow] = useState();
+
   return (
     <div className="App">
       <h1>Formik Form</h1>
-      <AboutUs />
+      <Box>
+        <Button label="show" onClick={() => setShow(true)} />
+        {show && (
+          <Layer
+            onEsc={() => setShow(false)}
+            onClickOutside={() => setShow(false)}
+          >
+            <AboutUs />
+            <StyledButton
+              size="small"
+              label="close"
+              onClick={() => setShow(false)}
+            />
+          </Layer>
+        )}
+      </Box>
+      {/* <Button primary label="Button" onClick={buttonClick} /> */}
     </div>
   );
 }
